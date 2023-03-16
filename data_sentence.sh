ae42a62788847b8100267f4e545859fa2adb1326
m1stborn <r10922178@ntu.edu.tw>
2023-03-17 03:19:00 +0800

    Feat: distribute training

---
+++ data_sentence.sh
@@ -0,0 +1,16 @@
+python explore_dataset.py \
+        --pretrained pretrained/HICO_GEN_VLKT_S.pth \
+        --dataset_file hico \
+        --hoi_path ../gen-vlkt/data/hico_20160224_det \
+        --num_obj_classes 80 \
+        --num_verb_classes 117 \
+        --backbone resnet50 \
+        --num_queries 100 \
+        --dec_layers 3 \
+        --eval \
+        --with_clip_label \
+        --with_obj_clip_label \
+        --use_nms_filter \
+        --thres_nms 0.7 \
+        --dev
+
